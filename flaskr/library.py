a867336f1299ed57771698fee2f47fa20805abdc
Jose Garcia <josegarcia@Joses-MBP.hsd1.ca.comcast.net>
2020-11-22 16:15:58 -0800

    Librarians can now view all registerd users

--- flaskr/library.py
+++ flaskr/library.py
@@ -8,8 +8,6 @@ from flaskr.db import get_db
 
 bp = Blueprint('library', __name__)
 
-# Right now i'm extracting all the users but we could, for example, extract all books associated with said user.
-
 def getUser():
     db = get_db()
     user = 'student'
@@ -21,33 +19,54 @@ def getUniversity():
     db = get_db()
     return db.execute('SELECT * FROM University WHERE un_id = ?',(g.user["u_universityid"],)).fetchone()
 
+def getAllBooks():
+    db = get_db()
+    return db.execute(
+    'SELECT * FROM Books, Author, Stockroom, University WHERE s_universityid = un_id AND b_isbn = s_isbn AND b_authorid = a_authorid AND s_universityid = ?',(g.user["u_universityid"],)
+    ).fetchall()
+def getFilteredBooks(filter, input):
+    db = get_db()
+    input = "%" + input + "%"
+    if filter == "title":
+        temp = 'b_title LIKE ?'
+    elif filter == 'author':
+        temp = 'a_authorname LIKE ?'
+    elif filter == 'year':
+        temp = 'b_publishedyear LIKE ?'
+    else:
+        temp = "b_isbn LIKE ?"
+
+    query = "SELECT * FROM Books, Author, Stockroom, University WHERE s_universityid = un_id AND b_isbn = s_isbn AND b_authorid = a_authorid AND s_universityid = ? AND " + temp
+
+    return db.execute(query,([g.user["u_universityid"], input])).fetchall()
+
+def getLibraryUsers():
+    db = get_db()
+    return db.execute('SELECT * FROM User, University WHERE u_universityid = un_id AND un_id = ?',(g.user["u_universityid"],)).fetchall()
+def getFilteredUsers(filter, input):
+    db = get_db()
+    input = "%" + input + "%"
+    if filter == "name":
+        temp = 'u_name LIKE ?'
+    else:
+        temp = 'u_email LIKE ?'
+    
+    query = "SELECT * FROM User, University WHERE u_universityid = un_id AND un_id = ? AND " + temp
+
+    return db.execute(query,([g.user["u_universityid"], input])).fetchall()
+
 @bp.route('/', methods=('GET', 'POST'))
 def index():
     if request.method == 'GET':
-        db = get_db()
-        books = db.execute(
-           'SELECT * FROM Books, Author, Stockroom, University WHERE s_universityid = un_id AND b_isbn = s_isbn AND b_authorid = a_authorid AND s_universityid = ?',(g.user["u_universityid"],)
-        ).fetchall()
-        return render_template('library/index.html', user=getUser(), university=getUniversity(), books=books)
-    
+        return render_template('library/index.html', user=getUser(), university=getUniversity(), books=getAllBooks())
+
     if request.method == 'POST':
-        db = get_db()
-        filter = request.form["filter"]
-        search = "%" + request.form['search'] + "%"
-        if filter == "title":
-            temp = 'b_title LIKE ?'
-        elif filter == 'author':
-            temp = 'a_authorname LIKE ?'
-        elif filter == 'year':
-            temp = 'b_publishedyear LIKE ?'
-        else:
-            temp = "b_isbn LIKE ?"
-
-        query = "SELECT * FROM Books, Author, Stockroom, University WHERE s_universityid = un_id AND b_isbn = s_isbn AND b_authorid = a_authorid AND s_universityid = ? AND " + temp
-
-        books = db.execute(
-            query,([g.user["u_universityid"], search])
-        ).fetchall()
-
-        return render_template('library/index.html', user=getUser(), university=getUniversity(), books=books)
-
+        button = request.form["button"]
+        if button == "refresh books":
+            return render_template('library/index.html', user=getUser(), university=getUniversity(), books=getAllBooks())
+        if button == "filter books":
+            return render_template('library/index.html', user=getUser(), university=getUniversity(), books=getFilteredBooks(request.form["filter"],request.form['input']))
+        if button == "refresh users" or button == "search users":
+            return render_template('library/index.html', user=getUser(), university=getUniversity(), libraryUsers=getLibraryUsers())
+        if button == "filter users":
+            return render_template('library/index.html', user=getUser(), university=getUniversity(), libraryUsers=getFilteredUsers(request.form["filter"],request.form['input']))
