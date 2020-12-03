a5fbcec8df36d67e93eda8c2b1bd27a5a20bdfe0
rsanchez-macias <rsanchez-macias@ucmerced.edu>
2020-12-02 16:37:56 -0800

    Add file to test new queries

---
+++ queries/test_queries.sql
@@ -0,0 +1,18 @@
+
+SELECT * 
+FROM Books, Author, Stockroom, University
+WHERE s_universityid = un_id AND 
+    b_isbn = s_isbn AND 
+    b_authorid = a_authorid AND
+    s_universityid = 1 AND
+    a_authorname like '%o%';
+
+
+select *
+from Author
+order by a_authorname;
+
+SELECT COUNT(*)
+FROM CheckedBooks
+WHERE cb_isbn = "60929871" AND 
+    cb_userid = 1;
