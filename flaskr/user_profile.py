4662e8d4db244eb4a43b5b4692824135489ffd9c
rsanchez-macias <rsanchez-macias@ucmerced.edu>
2020-12-05 15:36:31 -0800

    User able to "return" books

--- flaskr/user_profile.py
+++ flaskr/user_profile.py
@@ -5,13 +5,57 @@ from flask import (
 from werkzeug.exceptions import abort
 from sqlite3 import Error
 
-from flaskr.auth import login_required
 from flaskr.db import get_db
-
-from . import library
 
 
 bp = Blueprint('profile', __name__, url_prefix='/profile/')
+
+
+def removeCheckedEntry(_book_isbn):
+    db = get_db()
+
+    sql = """
+        DELETE FROM CheckedBooks
+        WHERE cb_isbn = ? AND 
+            cb_userid = ?
+    """
+
+    try:
+        args = [_book_isbn, g.user['u_userid']]
+        db.execute(sql, args)
+        db.commit()
+    except Error as e:
+        db.rollback()
+        print(e)
+
+
+def addOneBook(_book_isbn, _university_id):
+    db = get_db()
+
+    sql = """
+        UPDATE StockRoom
+        SET s_bookcount = (
+            SELECT s_bookcount + 1
+            FROM StockRoom, Books, University
+            WHERE b_isbn = s_isbn AND 
+                s_universityid = un_id AND 
+                s_universityid = ? AND 
+                s_isbn = ?)
+        WHERE s_isbn = ? AND 
+            s_universityid = ?
+    """
+
+    args = [_university_id, _book_isbn, _book_isbn, _university_id]
+
+    try:
+        db.execute(sql, args)
+        db.commit()
+    except Error as e:
+
+        db.rollback()
+        print(e)
+
+
 
 def getCheckedBooksForUser(_user_id):
     db = get_db()
@@ -34,11 +78,22 @@ def getCheckedBooksForUser(_user_id):
 
     return books
 
+
+@bp.route('/return_book', methods=('GET', 'POST'))
+def return_book():
+
+    if request.method == "GET":
+        isbn = request.args.get('data')
+        removeCheckedEntry(isbn)
+        addOneBook(isbn, g.user['u_universityid'])
+
+    return 'success'
+
+
 @bp.route('/', methods=('GET', 'POST'))
 def account():
 
     checked_books = getCheckedBooksForUser(g.user["u_userid"])
-    print(checked_books)
 
     account_info = [g.user['u_name'], g.user['u_email'], getUniversity()[1]]
 
