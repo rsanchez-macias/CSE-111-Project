7cf13f0f2fe25f54f91374851ae478c2eebb9904
rsanchez-macias <rsanchez-macias@ucmerced.edu>
2020-12-07 19:08:40 -0800

    User able to renew books and see their reservations

--- flaskr/user_profile.py
+++ flaskr/user_profile.py
@@ -1,3 +1,5 @@
+from datetime import date
+from flaskr.checkout import checked
 from flaskr.library import getUniversity
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
@@ -7,6 +9,9 @@ from sqlite3 import Error
 
 from flaskr.db import get_db
 
+from datetime import date
+from datetime import timedelta
+from datetime import datetime
 
 bp = Blueprint('profile', __name__, url_prefix='/profile/')
 
@@ -79,6 +84,101 @@ def getCheckedBooksForUser(_user_id):
     return books
 
 
+def getReservedBooksForUser(_user_id):
+    db = get_db()
+
+    sql = """
+        SELECT *
+        FROM ReservedBooks, Books, Author
+        WHERE r_isbn = b_isbn AND 
+            b_authorid = a_authorid AND
+            r_foruserid = ?
+    """
+
+    books = []
+
+    try:
+        cur = db.cursor()
+        books = cur.execute(sql, [_user_id]).fetchall()
+
+    except Error as e:
+        print(e)
+
+    return books
+
+
+def getDiffereceBetweenDates(_isbn, _user_id):
+
+    db = get_db()
+
+    sql = """
+        SELECT *
+        FROM CheckedBooks
+        WHERE cb_userid = ? AND 
+            cb_isbn = ?
+    """
+
+    dates = []
+
+    try:
+        cur = db.cursor()
+        dates = cur.execute(sql, [_user_id, _isbn]).fetchone()
+
+    except Error as e:
+        print(e)
+
+    d1 = datetime.strptime(dates[2].strftime("%Y-%m-%d"), "%Y-%m-%d")
+    d2 = datetime.strptime(dates[3].strftime("%Y-%m-%d"), "%Y-%m-%d")
+
+    return abs((d2 - d1).days)
+
+
+def updateReservationDate(_isbn, _user_id):
+
+    db = get_db()
+
+    sql = """
+        SELECT *
+        FROM CheckedBooks
+        WHERE cb_userid = ? AND 
+            cb_isbn = ?
+    """
+
+    checked = []
+
+    try:
+        cur = db.cursor()
+        checked = cur.execute(sql, [_user_id, _isbn]).fetchone()
+
+    except Error as e:
+        print(e)
+
+    
+    original_date = checked[3]
+    original_date = datetime.combine(original_date, datetime.min.time())
+    expiration_date = original_date + timedelta(days=30)
+
+    expiration_date = expiration_date.strftime('%Y-%m-%d')
+
+    sql = """
+        UPDATE CheckedBooks
+        SET cb_experiationdate = ?
+        WHERE cb_userid = ? AND 
+            cb_isbn = ?
+    """
+
+    try:
+        db.execute(sql, [expiration_date, _user_id, _isbn])
+        db.commit()
+
+    except Error as e:
+        db.rollback()
+        print(e)
+
+    return expiration_date
+
+
+
 @bp.route('/return_book', methods=('GET', 'POST'))
 def return_book():
 
@@ -90,11 +190,50 @@ def return_book():
     return 'success'
 
 
+@bp.route('/renew_book', methods=('GET', 'POST'))
+def renew_book():
+
+    if request.method == "GET":
+        isbn = request.args.get('data')
+        
+        diff = getDiffereceBetweenDates(isbn, g.user['u_userid'])
+        
+        if diff < 60:
+            new_date = updateReservationDate(isbn, g.user['u_userid'])
+            
+            return new_date
+        
+
+    return 'failure'
+
+
 @bp.route('/', methods=('GET', 'POST'))
 def account():
 
-    checked_books = getCheckedBooksForUser(g.user["u_userid"])
+    checked_books_raw = getCheckedBooksForUser(g.user["u_userid"])
+    reserved_books = getReservedBooksForUser(g.user["u_userid"])
 
+    checked_books = []
+    counter = 0
+   
+    for checked_book in checked_books_raw:
+        diff = getDiffereceBetweenDates(checked_book['cb_isbn'], checked_book['cb_userid'])
+        checked_books.append({})
+
+        checked_books[counter]['b_image_url'] = checked_book['b_image_url']
+        checked_books[counter]['b_title'] = checked_book['b_title']
+        checked_books[counter]['b_isbn'] = checked_book['b_isbn']
+        checked_books[counter]['a_authorname'] = checked_book['a_authorname']
+        checked_books[counter]['cb_checkeddate'] = checked_book['cb_checkeddate']
+        checked_books[counter]['cb_experiationdate'] = checked_book['cb_experiationdate']
+
+        if diff >= 60:
+            checked_books[counter]['disabled'] = 1
+        else:
+            checked_books[counter]['disabled'] = 0
+
+        counter += 1
+        
     account_info = [g.user['u_name'], g.user['u_email'], getUniversity()[1]]
 
-    return render_template('users/useraccount.html', account_info=account_info, checked_books=checked_books)
+    return render_template('users/useraccount.html', account_info=account_info, checked_books=checked_books, reserved_books=reserved_books)
